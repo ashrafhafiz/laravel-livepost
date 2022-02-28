99e17df3e72225f7241e4b4c13b306d452b088ab
Ashraf Hafiz <ashraf.hafiz@yahoo.com>
2022-02-28 16:55:44 +0200

    2nd Commit

---
+++ database/migrations/2022_02_28_242113_create_post_user_pivot_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostUserPivotTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_user', function (Blueprint $table) {
+            $table->unsignedBigInteger('post_id')->index();
+            $table->foreign('post_id')->references('id')->on('posts')->cascadeOnDelete();
+            $table->unsignedBigInteger('user_id')->index();
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->primary(['post_id', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_user');
+    }
+}
